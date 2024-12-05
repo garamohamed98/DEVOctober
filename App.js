509c3b32932870478e5e445b88d4b7e2c2247e58
Mohamed Gara <garamohamed9@gmail.com>
2024-12-05 19:32:09 +0100

    TP5 complited

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import { View } from "react-native";
 import ProductScreen from "./src/screens/ProductScreen";
+import ProductDetailsScreen from "./src/screens/ProductDetailsScreen";
 
 export default function App() {
   return (
     <View>
-      <ProductScreen />
+      <ProductDetailsScreen />
     </View>
   );
 }
